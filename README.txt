07b4a451994305fdf554ab69f7d36b62b0643ad9
Dimitra Sarantopoulou <demisarantopoulou@gmail.com>
2016-03-03 17:21:28 -0500

    export a file for each phase

--- README.txt
+++ README.txt
@@ -3,3 +3,4 @@ ruby fulllist2timepoints.rb all_samples_of_study_from_collos.csv
 
 Step 2: Generate all the files per subject you need to import to redcap. It uses the output of Step1, that is the collos_exports/{subject_name}/ directory. In to_import directory it generates a directory for each subejct, named as the subject. In the subject's directory it generates all the per phase_day files.
 ruby to_redcap.rb "30"
+
--- fulllist2timepoints.rb
+++ fulllist2timepoints.rb
@@ -19,13 +19,28 @@ CSV.foreach(ifile) do |row|
 	end
 end
 
+
+
 # split to separate files per subject, per phase, per day
 full_list.each do |subject, samples|
 	files_list.uniq.each do |f|
 		Dir.mkdir("collos_exports/#{subject}") unless File.exists?("collos_exports/#{subject}")
+		(fphase, fday) = f.split("_")
+
+		# export a file for each phase
+		CSV.open("collos_exports/#{subject}/#{fphase}.csv", "w") do |csv|
+			csv << ["sample_collos_id","sample_identifier","id_barcode","id_subject","id_study","sample_type","collection-time-point","treatment","sample_name","species","material_type","quantity","box_label","box_barcode","freezer_label","freezer_type","type"]
+			samples.each do |sample|
+				(phase, day, tp) = sample[:collection_time_point].split(" ")
+				if phase == fphase
+					csv << [sample[:sample_collos_id], sample[:sample_identifier], sample[:id_barcode], sample[:id_subject], sample[:id_study], sample[:sample_type], sample[:collection_time_point], sample[:treatment], sample[:sample_name], sample[:species], sample[:material_type], sample[:quantity], sample[:box_label], sample[:box_barcode], sample[:freezer_label], sample[:freezer_type], sample[:type]]
+				end
+			end
+		end
+
+		# split to separate files per subject, per phase, per day
 		CSV.open("collos_exports/#{subject}/#{f.downcase}.csv", "w") do |csv|
 			csv << ["sample_collos_id","sample_identifier","id_barcode","id_subject","id_study","sample_type","collection-time-point","treatment","sample_name","species","material_type","quantity","box_label","box_barcode","freezer_label","freezer_type","type"]
-			(fphase, fday) = f.split("_")
 			samples.each do |sample|
 				(phase, day, tp) = sample[:collection_time_point].split(" ")
 				if phase == fphase && day == fday
